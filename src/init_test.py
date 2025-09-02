9b9262099d318bfc56267b3081c6d6f50de71eb9
arvin-das <arvin.norway@gmail.com>
2025-09-02 18:39:47 +0200

    feat: made a smal test script to read inn the data

---
+++ src/init_test.py
@@ -0,0 +1,18 @@
+
+import os
+import matplotlib.pyplot as plt
+
+file_name = "Signal2_2018.csv"
+file_path = os.path.relpath(file_name)
+print(f"Relative path: {file_path}")
+
+with open(file_path, "r") as csv_file:
+    data = csv_file.read().strip().split(",")
+    data = [float(x) for x in data]
+
+plt.plot(data)
+plt.xlabel("Sample")
+plt.ylabel("Signal amplitude")
+plt.title("Raw signal")
+plt.grid()
+plt.show()
