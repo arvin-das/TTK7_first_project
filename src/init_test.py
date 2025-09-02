e05a43449856cb37bb25a03ca4b54e7852e0ab7f
arvin-das <arvin.norway@gmail.com>
2025-09-02 19:02:04 +0200

    feat: fixed the structure of the project

--- src/init_test.py
+++ src/init_test.py
@@ -2,6 +2,8 @@
 import os
 import matplotlib.pyplot as plt
 
+from transforms import fft
+
 file_name = "Signal2_2018.csv"
 file_path = os.path.relpath(file_name)
 print(f"Relative path: {file_path}")
@@ -9,6 +11,8 @@ print(f"Relative path: {file_path}")
 with open(file_path, "r") as csv_file:
     data = csv_file.read().strip().split(",")
     data = [float(x) for x in data]
+
+print(fft.test())
 
 plt.plot(data)
 plt.xlabel("Sample")
