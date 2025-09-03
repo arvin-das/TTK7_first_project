55cb7ed3be2b1070524f0deddc3e3f33e6cf0071
arvin-das <arvin.norway@gmail.com>
2025-09-04 00:47:24 +0200

    feat: made small changes to the fft code

--- src/init_test.py
+++ src/init_test.py
@@ -1,8 +1,11 @@
-
 import os
 import matplotlib.pyplot as plt
-
 from transforms import fft
+import numpy as np
+
+Fs = 100 
+
+
 
 file_name = "Signal2_2018.csv"
 file_path = os.path.relpath(file_name)
@@ -12,11 +15,22 @@ with open(file_path, "r") as csv_file:
     data = csv_file.read().strip().split(",")
     data = [float(x) for x in data]
 
-print(fft.test())
+time_axis = np.linspace(0, len(data)/Fs, len(data))
 
-plt.plot(data)
-plt.xlabel("Sample")
+plt.plot(time_axis, data)
+plt.xlabel("Time (s)")
 plt.ylabel("Signal amplitude")
 plt.title("Raw signal")
+plt.grid()
+plt.show()
+
+fft_data = fft.fft_transform(data)
+fft_axis = np.fft.fftfreq(len(data), 1/Fs)
+
+plt.figure()
+plt.plot(fft_axis, fft_data)
+plt.xlabel("Frequency (Hz)")
+plt.ylabel("Magnitude")
+plt.title("FFT of the signal")
 plt.grid()
 plt.show()
--- src/transforms/fft.py
+++ src/transforms/fft.py
@@ -1,3 +1,8 @@
+from scipy.fft import fft
+
+def fft_transform(data):
+    """Apply Fast Fourier Transform to the input data."""
+    return fft(data)
 
 def test():
     return "test"
