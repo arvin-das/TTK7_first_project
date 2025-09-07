906f7281c5688d32473fbcbdd6fca3a6590609aa
Christopher Chen <christopher.yx.chen@gmail.com>
2025-09-07 20:53:19 +0200

    Non-Analytic Analysers

--- signal_playground.py
+++ signal_playground.py
@@ -1,6 +1,7 @@
 import numpy as np
 from src.signal_mod.signal_modifier import SignalModifier
 from src.transforms.analytical import AnalyticalAnalyser
+from src.transforms.non_analytical import NonAnalyticalAnalyser
 import matplotlib.pyplot as plt
 
 SAMPLING_FREQUENCY = 100 # found in signal generation script
@@ -22,13 +23,39 @@ modifier.add_component(0.3)
 
 fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
 time_axis = np.linspace(0, len(raw_signal)/SAMPLING_FREQUENCY, len(raw_signal))
+sampling_period = np.diff(time_axis).mean()
 ax1.plot(time_axis, modifier.get_original_signal())
 ax2.plot(time_axis, modifier.get_modified_signal())
 ax2.set_xlabel("Time [s]")
 plt.show()
 
+
 analytical_analyser = AnalyticalAnalyser(modifier.get_original_signal())
 wv = analytical_analyser.wigner_ville_distribution()
-
 wv.plot(kind='contour', show_tf=True)
 
+
+non_analytical_analyser = NonAnalyticalAnalyser(modifier.get_original_signal(), sampling_period)
+fft = non_analytical_analyser.fast_fourier()
+x_fft = np.fft.rfftfreq(len(modifier.get_original_signal()), sampling_period)
+plt.plot(x_fft, fft)
+plt.title('FFT')
+plt.ylabel('Energy')
+plt.xlabel('Frequency (Hz)')
+plt.grid()
+plt.show()
+
+
+stft = non_analytical_analyser.short_time_fourier()
+stft.plot()
+
+
+cwtmatr, freqs = non_analytical_analyser.wavelet_transform()
+fig, axs = plt.subplots(1, 1)
+pcm = axs.pcolormesh(time_axis, freqs, cwtmatr)
+axs.set_yscale("log")
+axs.set_xlabel("Time (s)")
+axs.set_ylabel("Frequency (Hz)")
+axs.set_title("Continuous Wavelet Transform (Scaleogram)")
+fig.colorbar(pcm, ax=axs)
+plt.show()
--- src/transforms/non_analytical.py
+++ src/transforms/non_analytical.py
@@ -1,14 +1,29 @@
-def NonAnalyticalAnalyser():
-    def __init__(self, signal):
+import numpy as np
+from tftb.processing.linear import ShortTimeFourierTransform
+import pywt
+
+class NonAnalyticalAnalyser:
+    def __init__(self, signal, sampling_period):
         self.signal = signal
+        self.sampling_period = sampling_period
     
-    def fast_fourier():
-        pass
+    def fast_fourier(self):
+        return np.fft.rfft(self.signal)
     
-    def short_time_fourier():
-        pass
+    def short_time_fourier(self):
+        analysis = ShortTimeFourierTransform(self.signal)
+        analysis.run()
+        return analysis
     
-    def wavelet_transform():
-        pass
+    def wavelet_transform(self):
+        # Wavelet type
+        wavelet = "cmor1.5-1.0"
+        # logarithmic scale for scales, as suggested by Torrence & Compo (A paper):
+        widths = np.geomspace(1, 1024, num=100)
+        cwtmatr, freqs = pywt.cwt(self.signal, widths, wavelet, sampling_period=self.sampling_period)
+        # absolute take absolute value of complex result
+        cwtmatr = np.abs(cwtmatr[:-1, :-1])
+        return cwtmatr, freqs
+        
     
     
